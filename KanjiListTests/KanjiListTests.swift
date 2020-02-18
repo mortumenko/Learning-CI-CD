c1ea2fee7a3b7b013a9c736656e1dbeba867cdc9
Oleksandr Sulyma <>
2020-02-18 16:35:16 +0200

    Fastfile was updated

--- KanjiListTests/KanjiListTests.swift
+++ KanjiListTests/KanjiListTests.swift
@@ -14,17 +14,16 @@ class KanjiListTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+/*
     func testFailured() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(3 == 2, "❌ Everything is bad")
-    }
+    }*/
 
     func testPassed() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
         XCTAssert(1 == 1, "❌ Everything is bad")
     }
     
