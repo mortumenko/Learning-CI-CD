8cdedc8fbc2487cdcf9286ccafd2f39ff7402488
Oleksandr Sulyma <>
2020-02-27 16:42:32 +0200

    Changes-1 as Jenkins trigger

--- KanjiListUITests/KanjiListUITests.swift
+++ KanjiListUITests/KanjiListUITests.swift
@@ -41,6 +41,7 @@ class KanjiListUITests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        //is it trigger?
     }
 
     func testExample() {
