a9c782e366fa326701c4442c2f42037cf02acba9
Oleksandr Sulyma <>
2020-02-28 15:53:38 +0200

    Revert "Added failure-able test"
    This reverts commit 6cb1d016586978bdfb451f027f45cce432bc2b96.

--- KanjiListTests/KanjiListTests.swift
+++ KanjiListTests/KanjiListTests.swift
@@ -14,12 +14,12 @@ class KanjiListTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+/*
     func testFailured() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(3 == 2, "❌ Everything is bad")
-    }
+    }*/
 
     func testPassed() {
         // This is an example of a functional test case.
